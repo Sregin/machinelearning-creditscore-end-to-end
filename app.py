1016dcc90e998877321760bf3dafcf28279fac51
Sregin <146487662+Sregin@users.noreply.github.com>
2024-05-19 23:34:25 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+
+model = joblib.load('svm_model.pkl')
+preprocessor = joblib.load('pipeline.pkl')  
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame(data, index=[0])
+    df_processed = preprocessor.transform(df)
+    prediction = model.predict(df_processed)
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
